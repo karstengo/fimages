3d475dd2160ec494b12423ec3f2fcb9c44c8153f
ogak <ogak@home.pgr>
2020-02-02 20:59:59 +0100

    fix port

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static')
 app.IMAGEBASEPATH='images/'
-app.run(debug=True)
+app.run(debug=True, port=5000)
 
 import logging
 logging.basicConfig(filename='debug.log',level=logging.DEBUG)
